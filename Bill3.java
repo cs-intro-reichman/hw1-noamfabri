cd84d914b5532c5902c5d347ac56a7d259b0242b
noamfabri87 <noamfabri87@gmail.com>
2025-10-30 22:04:37 +0200

    "Bill3 is Done"

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,12 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	    String name2 = args[1];
+		String name3 = args[2];
+		int bill =  Integer.parseInt(args[3]);
+		double billPerPerson = Math.ceil(bill / 3.0);
+		System.out.println("Dear " + name1 + ", " + name2 + ", and " +name3 +
+						   ": pay " + billPerPerson + " Shekels each.");
+		
 	}
 }
